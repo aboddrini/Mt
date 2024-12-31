1ca680088185a8248cb726936b05a835435747e6
aboddrini <aboddrini99999@gmail.com>
2024-12-31 15:58:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Function to toggle the sidebar
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const mainContent = document.getElementById('main-content');
+    
+    if (sidebar.style.width === '250px') {
+        sidebar.style.width = '0';
+        mainContent.style.marginLeft = '0';
+    } else {
+        sidebar.style.width = '250px';
+        mainContent.style.marginLeft = '250px';
+    }
+}
